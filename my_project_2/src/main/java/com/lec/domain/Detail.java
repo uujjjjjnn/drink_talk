db431f44d4d8ed4162b1dc2915567a10d8de8c2a
uujjjjjnn <uujjjjjnn@gmail.com>
2023-09-17 21:53:25 +0900

    detail 수정

--- my_project_2/src/main/java/com/lec/domain/Detail.java
+++ my_project_2/src/main/java/com/lec/domain/Detail.java
@@ -1,5 +1,22 @@
 package com.lec.domain;
 
-public class Detail {
+import javax.persistence.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Detail {
+	
+	private Double flavorSweet; // 단맛
+	private Double flavorBody; // 바디감
+	private Double flavorPop; // 청량감
+	private Double flavorSour; // 신맛
+
+	
+	
 }
